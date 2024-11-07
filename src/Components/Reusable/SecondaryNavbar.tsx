65330b2e08ec40c47368a25719c1ceec506ae648
Raja Shamir <153479520+shamirz123@users.noreply.github.com>
2024-11-07 18:27:38 +0500

    service countries and megamenu

--- src/Components/Reusable/SecondaryNavbar.tsx
+++ src/Components/Reusable/SecondaryNavbar.tsx
@@ -13,11 +13,14 @@ import { IoLocation } from "react-icons/io5";
 import { RiPinterestFill } from "react-icons/ri";
 import { Drawer, IconButton } from "@mui/material";
 import Image from "next/image";
+import { MdLocationPin } from "react-icons/md";
 
 interface Breadcrumbs {
   name: string;
   path: string;
 }
+
+
 
 interface SecondaryNavbarProps {
   heading: string;
@@ -29,6 +32,170 @@ interface Language {
   name: string;
   flag: string;
 }
+
+const sections = [
+  {
+    title: "Oceania",
+    links: [
+      { label: "American Samoa", icon: <MdLocationPin /> },
+      { label: "Cook Islands", icon: <MdLocationPin /> },
+      { label: "Fiji", icon: <MdLocationPin /> },
+      { label: "French Polynesia", icon: <MdLocationPin /> },
+      { label: "Guadeloupe", icon: <MdLocationPin /> },
+      { label: "Kiribati", icon: <MdLocationPin /> },
+      { label: "Marshall Islands", icon: <MdLocationPin /> },
+      { label: "Micronesia", icon: <MdLocationPin /> },
+      { label: "New Caledonia", icon: <MdLocationPin /> },
+      { label: "New Zealand", icon: <MdLocationPin /> },
+      { label: "Niue", icon: <MdLocationPin /> },
+      {
+        label: "Northern Marianas Islands",
+        icon: <MdLocationPin />,
+        href: "#",
+      },
+      { label: "Papua New Guinea", icon: <MdLocationPin /> },
+      { label: "Niue", icon: <MdLocationPin /> },
+      { label: "Solomon Islands", icon: <MdLocationPin /> },
+      { label: "Tokelau", icon: <MdLocationPin /> },
+      { label: "Tonga Islands", icon: <MdLocationPin /> },
+      { label: "Tuvalu", icon: <MdLocationPin /> },
+      { label: "Vanuatu", icon: <MdLocationPin /> },
+      {
+        label: "Wallis and Futuna Islands",
+        icon: <MdLocationPin />,
+        href: "#",
+      },
+      { label: "Western Samoa", icon: <MdLocationPin /> },
+    ],
+  },
+  {
+    title: "North America",
+    links: [
+      { label: "Anguilla", icon: <MdLocationPin /> },
+      { label: "Antigua and Barbuda", icon: <MdLocationPin /> },
+      { label: "Bahamas", icon: <MdLocationPin /> },
+      { label: "Barbados", icon: <MdLocationPin /> },
+      { label: "Belize", icon: <MdLocationPin /> },
+      { label: "Bermuda", icon: <MdLocationPin /> },
+      { label: "British Virgin Islands", icon: <MdLocationPin /> },
+      { label: "Canada", icon: <MdLocationPin /> },
+      { label: "Costa Rica", icon: <MdLocationPin /> },
+      { label: "Cuba", icon: <MdLocationPin /> },
+      { label: "Dominica", icon: <MdLocationPin /> },
+      { label: "Dominican Republic", icon: <MdLocationPin /> },
+      { label: "Grenada", icon: <MdLocationPin /> },
+      { label: "Guam", icon: <MdLocationPin /> },
+      { label: "Guatemala", icon: <MdLocationPin /> },
+      { label: "Haiti", icon: <MdLocationPin /> },
+      { label: "Honduras", icon: <MdLocationPin /> },
+      { label: "Jamaica", icon: <MdLocationPin /> },
+      { label: "Martinique", icon: <MdLocationPin /> },
+      { label: "Mexico", icon: <MdLocationPin /> },
+      { label: "Montserrat", icon: <MdLocationPin /> },
+      { label: "Nicaragua", icon: <MdLocationPin /> },
+      { label: "Panama", icon: <MdLocationPin /> },
+      { label: "Puerto Rico", icon: <MdLocationPin /> },
+      { label: "Saint Kitts and Nevis", icon: <MdLocationPin /> },
+      { label: "Saint Lucia", icon: <MdLocationPin /> },
+      { label: "Saint Pierre and Miquelon", icon: <MdLocationPin /> },
+      {
+        label: "Saint Vincent and the Grenadines",
+        icon: <MdLocationPin />,
+        href: "#",
+      },
+      { label: "Salvador", icon: <MdLocationPin /> },
+      { label: "Trinidad and Tobago", icon: <MdLocationPin /> },
+      { label: "Turks and Caicos Islands", icon: <MdLocationPin /> },
+      { label: "United States of America", icon: <MdLocationPin /> },
+      { label: "US Virgin Islands", icon: <MdLocationPin /> },
+      { label: "Honduras", icon: <MdLocationPin /> },
+    ],
+  },
+  {
+    title: "South America",
+    links: [
+      { label: "Argentina", icon: <MdLocationPin /> },
+      { label: "Aruba", icon: <MdLocationPin /> },
+      { label: "Bolivia", icon: <MdLocationPin /> },
+      { label: "Brazil", icon: <MdLocationPin /> },
+      { label: "Chile", icon: <MdLocationPin /> },
+      { label: "Colombia", icon: <MdLocationPin /> },
+      { label: "Ecuador", icon: <MdLocationPin /> },
+      { label: "Falkland Islands", icon: <MdLocationPin /> },
+      { label: "French Guiana", icon: <MdLocationPin /> },
+      { label: "Guyana", icon: <MdLocationPin /> },
+      { label: "Paraguay", icon: <MdLocationPin /> },
+      { label: "Peru", icon: <MdLocationPin /> },
+      { label: "Suriname", icon: <MdLocationPin /> },
+      { label: "Uruguay", icon: <MdLocationPin /> },
+      { label: "Venezuela", icon: <MdLocationPin /> },
+    ],
+  },
+  {
+    title: "Europe",
+    links: [
+      { label: "Andorra", icon: <MdLocationPin /> },
+      { label: "Armenia", icon: <MdLocationPin /> },
+      { label: "Austria", icon: <MdLocationPin /> },
+      { label: "Belarus", icon: <MdLocationPin /> },
+      { label: "Belgium", icon: <MdLocationPin /> },
+      { label: "Bulgaria", icon: <MdLocationPin /> },
+      { label: "Croatia", icon: <MdLocationPin /> },
+      { label: "Cyprus", icon: <MdLocationPin /> },
+      { label: "Czech Republic", icon: <MdLocationPin /> },
+      { label: "Denmark", icon: <MdLocationPin /> },
+      { label: "Estonia", icon: <MdLocationPin /> },
+      { label: "Faroe Islands", icon: <MdLocationPin /> },
+      { label: "Finland", icon: <MdLocationPin /> },
+      { label: "France", icon: <MdLocationPin /> },
+      { label: "Georgia", icon: <MdLocationPin /> },
+      { label: "Germany", icon: <MdLocationPin /> },
+      { label: "Gibraltar", icon: <MdLocationPin /> },
+      { label: "Greece", icon: <MdLocationPin /> },
+      { label: "Hungary", icon: <MdLocationPin /> },
+      { label: "Iceland", icon: <MdLocationPin /> },
+      { label: "Ireland", icon: <MdLocationPin /> },
+      { label: "Italy", icon: <MdLocationPin /> },
+      { label: "Latvia", icon: <MdLocationPin /> },
+      { label: "Liechtenstein", icon: <MdLocationPin /> },
+      { label: "Lithuania", icon: <MdLocationPin /> },
+      { label: "Luxembourg", icon: <MdLocationPin /> },
+      { label: "Malta", icon: <MdLocationPin /> },
+      { label: "Moldavia", icon: <MdLocationPin /> },
+      { label: "Monaco", icon: <MdLocationPin /> },
+      { label: "Netherlands", icon: <MdLocationPin /> },
+      { label: "Romania", icon: <MdLocationPin /> },
+      { label: "Portugal", icon: <MdLocationPin /> },
+      { label: "Poland", icon: <MdLocationPin /> },
+      { label: "San Marino", icon: <MdLocationPin /> },
+      { label: "Serbia", icon: <MdLocationPin /> },
+      { label: "Spain", icon: <MdLocationPin /> },
+      { label: "St. Helena", icon: <MdLocationPin /> },
+      { label: "Sweden", icon: <MdLocationPin /> },
+      { label: "Switzerland", icon: <MdLocationPin /> },
+      { label: "Ukraine", icon: <MdLocationPin /> },
+      { label: "United Kingdom", icon: <MdLocationPin /> },
+    ],
+  },
+  {
+    title: "Africa",
+    links: [
+      { label: "Eswatini (Swaziland)", icon: <MdLocationPin /> },
+      { label: "Botswana", icon: <MdLocationPin /> },
+      { label: "Lesotho", icon: <MdLocationPin /> },
+      { label: "Mayotte Island", icon: <MdLocationPin /> },
+      { label: "Reunion Island", icon: <MdLocationPin /> },
+      { label: "Seychelles", icon: <MdLocationPin /> },
+    ],
+  },
+  {
+    title: "Asia",
+    links: [
+      { label: "Japan", icon: <MdLocationPin /> },
+      { label: "South Korea", icon: <MdLocationPin /> },
+    ],
+  },
+];
 
 const socialLinks = [
   { icon: <FaTwitter />, url: "#" },
@@ -44,56 +211,7 @@ const menuItems = [
   {
     name: "Countries",
     path: "/serviced-countries",
-    megamenu: [
-      {
-        title: "Slider Home",
-        links: [
-          { name: "Full width Slider", path: "index.html" },
-          { name: "Full screen Slider", path: "index_03.html" },
-          { name: "Video Intro", path: "index_05.html" },
-          { name: "Kenburns Slider", path: "index_06.html" },
-          { name: "Parallax Slider", path: "index_10.html" },
-          { name: "Boxed Slider", path: "index_07.html" },
-          { name: "More examples", path: "_sliders.html", badge: "+" },
-          { name: "Coming Soon", path: "page_coming_soon.html" },
-        ],
-      },
-      {
-        title: "Parallax Home",
-        links: [
-          { name: "Full width Parallax Image", path: "index_04.html" },
-          { name: "Full screen Parallax Image", path: "index_12.html" },
-          { name: "Vertical Menu Boxed", path: "index_02.html" },
-          { name: "Fixed Vertical Menu", path: "index_09.html" },
-          { name: "Push vertical Menu", path: "index_06.html" },
-          { name: "More examples", path: "_menus.html", badge: "+" },
-        ],
-      },
-      {
-        title: "Header Home",
-        links: [
-          { name: "Floating Header", path: "index_05.html" },
-          { name: "Floating top bar Header", path: "index.html" },
-          { name: "Transparent Header", path: "index_03.html" },
-          { name: "Full width Header", path: "index_10.html" },
-          { name: "Top bar Header", path: "index_08.html" },
-          { name: "Bottom Header", path: "index_12.html" },
-          { name: "Simple Header", path: "index_07.html" },
-          { name: "More examples", path: "_headers.html", badge: "+" },
-        ],
-      },
-      {
-        title: "Boxed Home",
-        links: [
-          { name: "Large Boxed Content", path: "index_13.html" },
-          { name: "Boxed Content", path: "index_11.html" },
-          { name: "Onepage Full width Slider", path: "onepage.html" },
-          { name: "Onepage Full Screen Slider", path: "onepage_02.html" },
-          { name: "Onepage Parallax Image", path: "onepage_03.html" },
-          { name: "Onepage Fixed Vertical Menu", path: "onepage_04.html" },
-        ],
-      },
-    ],
+    megamenu: sections,
   },
   { name: "Apply Now", path: "/apply-now" },
   {
@@ -274,20 +392,10 @@ const SecondaryNavbar: React.FC<SecondaryNavbarProps> = ({
                           </li>
 
                           {menuItems.map((item, index) => (
-                            <li
-                              key={index}
-                              className={
-                                item.megamenu
-                                  ? "has-megamenu"
-                                  : item.submenu
-                                  ? "has-submenu"
-                                  : ""
-                              }
-                            >
+                            <li key={index}>
                               <Link href={item.path}>{item.name}</Link>
                               {/* Render Megamenu if present */}
-
-                              {/* {item.megamenu && (
+                              {item.megamenu && (
                                 <div className="megamenu">
                                   <div className="row">
                                     {item.megamenu.map((section, secIndex) => (
@@ -299,8 +407,13 @@ const SecondaryNavbar: React.FC<SecondaryNavbarProps> = ({
                                           {section.links.map(
                                             (link, linkIndex) => (
                                               <li key={linkIndex}>
-                                                <Link href={link.path}>
-                                                  {link.name}
+                                                <Link href="">
+                                                  <div className="d-flex align-items-center">
+                                                    <div className="icon me-2">
+                                                      {link.icon}
+                                                    </div>
+                                                    <span>{link.label}</span>
+                                                  </div>
                                                 </Link>
                                               </li>
                                             )
@@ -310,8 +423,7 @@ const SecondaryNavbar: React.FC<SecondaryNavbarProps> = ({
                                     ))}
                                   </div>
                                 </div>
-                              )} */}
-
+                              )}
                               {item.submenu && (
                                 <ul className="submenu">
                                   {item.submenu.map((subItem, subIndex) => (
